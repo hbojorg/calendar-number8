ec0b418218f239c24cabb18f9536df9822ab0222
Jorge Huanca Borges <hbo.jorg@gmail.com>
2017-09-28 01:10:47 -0400

    Calculation of endDate using startDate and numberOfDays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DayModel } from './calendar/day.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my exercise UI DEV';
+  
+  public startDate: string;
+  public numberOfDays: string;
+  public countryCode: string;
+  public endDate: Date;
+
+  private patternDate: RegExp;
+  public _endDate: Date;
+  private _startDate: Date;
+
+  constructor() {
+    this.patternDate = /(\d{2})[/](\d{2})[/](\d{4})/;
+    this.startDate = "15/08/2017";
+    this.numberOfDays = "300";
+    this.setEndDate();
+  }
+
+  setEndDate() {
+    let startDateArray = this.patternDate.exec(this.startDate);
+    this._startDate = new Date(+startDateArray[3], (+startDateArray[2]-1), +startDateArray[1]);
+    this._endDate = new Date(this._startDate.getFullYear(), this._startDate.getMonth(), this._startDate.getDate());
+    this._endDate.setDate(this._endDate.getDate() + Number(this.numberOfDays));    
+  }
+
+
 }
